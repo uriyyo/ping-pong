c309cf42f8f24d0234604ae8c683c26eb47adb03
uriyyo <1998uriyyo@gmail.com>
2020-02-17 09:53:53 +0200

    Update type annotations

--- ping_pong/ui/consts.py
+++ ping_pong/ui/consts.py
@@ -3,20 +3,20 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from .types import Size, Color
 
-WIDTH: int = 1000
-HEIGHT: int = 650
+WIDTH: "int" = 1000
+HEIGHT: "int" = 650
 
-SIZE: Size = (WIDTH, HEIGHT)
+SIZE: "Size" = (WIDTH, HEIGHT)
 
-BLACK: Color = (0, 0, 0)
-WHITE: Color = (255, 255, 255)
+BLACK: "Color" = (0, 0, 0)
+WHITE: "Color" = (255, 255, 255)
 
-PADDLE_SPEED: int = 20
+PADDLE_SPEED: "int" = 20
 
-PADDLE_WIDTH: int = 20
-PADDLE_HEIGHT: int = 300
+PADDLE_WIDTH: "int" = 20
+PADDLE_HEIGHT: "int" = 300
 
-BALL_SIZE: int = 20
+BALL_SIZE: "int" = 20
 
 __all__ = [
     "WIDTH",
